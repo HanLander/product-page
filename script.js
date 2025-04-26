6292b57b1478340d789f79419e621df132cee674
HanLander <157143716+HanLander@users.noreply.github.com>
2025-04-26 10:44:16 +0100

    Added theme change button, JS and CSS

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
+
 function Submit(event) {
   event.preventDefault();
   alert("Your email address was submitted");
